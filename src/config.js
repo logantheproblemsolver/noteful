8aac6936761c3f1ab273cd1a796856014dac7d2c
Logan Ellis <mrloganellis@gmail.com>
2020-03-04 12:02:12 -0500

    fixed config file issue

--- src/config.js
+++ src/config.js
@@ -1,4 +1,4 @@
 export default {
-    API_ENDPOINT: `https://https://glacial-hamlet-38215.herokuapp.com/`,
+    API_ENDPOINT: `https://https://glacial-hamlet-38215.herokuapp.com`,
     API_KEY: process.env.REACT_APP_API_KEY,
 }
